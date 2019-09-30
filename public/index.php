34330e645e20f1b25874a1a8dd97cd0d080708e0
Valdis Saules <valdis.s.coding@gmail.com>
2019-09-30 13:06:13 +0300

    added constant example

--- public/index.php
+++ public/index.php
@@ -1,2 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP main</title>
+</head>
+<body>
+    <h1>PHP examples</h1>
 <?php
-    echo "It works!";
+    const MYSECRET = "foobar";
+    $myname = "Valdis";
+    $mynum = 25;
+    echo "<div>Hello $myname your number is $mynum. Your secret is " 
+        . MYSECRET . 
+        " Thank You!</div>";
+
+?>
+<main>More content</main>
+<footer>Created by <?php echo $myname; ?> </footer>
+</body>
+</html>
