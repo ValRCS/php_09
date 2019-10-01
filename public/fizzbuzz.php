d56b1ecafaece5fabbe139cbdaaf6cf0c8053666
Valdis Saules <valdis.s.coding@gmail.com>
2019-10-01 12:22:22 +0300

    Fizzbuzz in php

--- public/fizzbuzz.php
+++ public/fizzbuzz.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Fizzbuzz</title>
+    <link rel="stylesheet" href="styles/fizzbuzz.css">
+</head>
+<body>
 <?php
     //write fizzbuzz "Fizz" when divides by 3
     //"Buzz" when by 5
@@ -46,4 +56,8 @@
         }
 
     echo "</ol>";
+?>
+</body>
+</html>
+
 
