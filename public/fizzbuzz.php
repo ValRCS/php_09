ceeb900c10c05908f93c862baaa5ab50e0be97c5
Valdis Saules <valdis.s.coding@gmail.com>
2019-10-01 12:16:55 +0300

    FizzBuzz with classes

--- public/fizzbuzz.php
+++ public/fizzbuzz.php
@@ -23,8 +23,25 @@
         //lets not write directly :)
         // echo "<li>Number 1</li>";
         //write a for loop !
-        for ($i = 0; $i < 10; $i++) {
-            echo $i . "<br>";
+        $fizz = 3;
+        $buzz = 5;
+        for ($i = 1; $i <= 100; $i++) {
+            
+            if ($i % $fizz == 0 && $i % $buzz == 0) {
+                $innerText = "FizzBuzz";
+                $class = "fizz-buzz";
+            } else if ($i % $fizz == 0) {
+                $innerText = "Fizz";
+                $class = "fizz";
+            } else if ($i % $buzz == 0) {
+                $innerText = "Buzz";
+                $class = "buzz";
+            } else {
+                $innerText = "Number $i";
+                $class = "number";
+            }
+
+            echo "<li class='$class'>" . $innerText . "</li>";
             //add if and if ele here
         }
 
