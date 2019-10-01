18057a8d09927f36e1110142deb92909243f6f58
Valdis Saules <valdis.s.coding@gmail.com>
2019-10-01 11:56:45 +0300

    Fizzbuzz exercise

--- public/fizzbuzz.php
+++ public/fizzbuzz.php
@@ -2,10 +2,22 @@
     //write fizzbuzz "Fizz" when divides by 3
     //"Buzz" when by 5
     //"Fizzbuzz" when by 3 and 5
-    $a = 21;
-    $b = 4;
-    if ($a % $b == 0) {
-        echo "$a divides by $b";
-    } else {
-        echo "$a DOES not divide by $b";
-    }
+    // $a = 21;
+    // $b = 4;
+    // if ($a % $b == 0) {
+    //     echo "$a divides by $b";
+    // } else {
+    //     echo "$a DOES not divide by $b";
+    // }
+
+    //print fizzbuzz rezults in an ordered list
+    //so like
+    // 1. Number 1
+    // 2. Number 2
+    // 3. Fizz
+    // 4. Number 4
+    // 5. Buzz
+    // ...
+    // 15. FizzBuzz
+
+    
