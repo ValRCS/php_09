d8e2f0e90ad6f127940faf0dad5765c59c98d570
Valdis Saules <valdis.s.coding@gmail.com>
2019-10-03 11:42:03 +0300

    added a way to print table

--- public/mymusic.php
+++ public/mymusic.php
@@ -24,23 +24,75 @@ $result = $conn->query($sql);
 echo "<hr>";
 if ($result->num_rows > 0) {
     // output data of each row
-    while($row = $result->fetch_assoc()) {
-        //for now we use hardcoded keys since we know we have id and name and created
-        $trackText =  "<div class='my-trk'>id: " . $row["id"];
-        $trackText .= " - Name: " . $row["name"];
-        $trackText .= " - Artist: " . $row["artist"];
-        $trackText .=  " " . $row["created"]. "</div>";
-        echo $trackText;
+    //we can get ALL rows at once
+    //we use MYSQLI_ASSOC to ask for only associate array(without numeric keys)
+    $myrows = $result->fetch_all(MYSQLI_ASSOC);
+    var_dump($myrows);
+    echo "<hr>";
+    foreach ($myrows as $key => $row) {
+        echo "Processing Row No. $key <br>";
         //without extra variable, same result
         echo "<div class='my-trk'>id: " . $row["id"] 
             . " - Name: " . $row["name"] 
             . " - Artist: " . $row["artist"] 
             . " " . $row["created"]. "</div>";
+        echo "<hr>";
     }
+    //or we can get them row by row
+    // while($row = $result->fetch_assoc()) {
+    //     //for now we use hardcoded keys since we know we have id and name and created
+    //     $trackText =  "<div class='my-trk'>id: " . $row["id"];
+    //     $trackText .= " - Name: " . $row["name"];
+    //     $trackText .= " - Artist: " . $row["artist"];
+    //     $trackText .=  " " . $row["created"]. "</div>";
+    //     echo $trackText;
+    //     echo "<br>";
+    //     //without extra variable, same result
+    //     echo "<div class='my-trk'>id: " . $row["id"] 
+    //         . " - Name: " . $row["name"] 
+    //         . " - Artist: " . $row["artist"] 
+    //         . " " . $row["created"]. "</div>";
+    //     echo "<hr>";
+    // }
+    
 } else {
     echo "0 results";
 }
 echo "<hr>";
 //we could close before automatic close at the end of program
 $conn->close();
+
+
+//we can use $myrows as many as we want even after closing the database
+foreach ($myrows as $row) {
+    //without extra variable, same result
+    echo "<div class='my-trk'>id: " . $row["id"] 
+        . " - Name: " . $row["name"] 
+        . " " . $row["created"]. "</div>";
+    echo "<hr>";
+}
+
+//lets print a table of our results
+
+echo "<table>";
+foreach ($myrows as $key => $row) {
+    //special case using first row we create our table head with column names
+    if ($key == 0) {
+        echo "<thead><tr>";
+        foreach ($row as $colname => $unusedcolvalue) {
+            echo "<td>" . $colname . "</td>";
+        }
+        echo "</tr></thead>";
+    }
+    echo "<tr>";
+    // foreach ($row as $unusedcolkey => $colvalue) {
+    foreach ($row as  $cellvalue) {
+        
+        echo "<td>" . $cellvalue . "</td>";
+
+    }
+    echo "</tr>";
+}
+echo "</table>";
+
 require_once("../src/templates/footer.php");
