0a7e2c1cde38c00d0c815e936891a7d036ae06ee
Valdis Saules <valdis.s.coding@gmail.com>
2019-10-03 10:36:49 +0300

    added a quick and dirty header dynamism

--- public/mymusic.php
+++ public/mymusic.php
@@ -1,7 +1,8 @@
 <?php
+require_once("../config/config.php");
 require_once("../src/templates/header.php");
 //we want to store config separately and keep this file out of public git
-require_once("../config/config.php");
+
 
 // Create connection
 $conn = mysqli_connect(SERVERNAME, USERNAME, PW, DB);
@@ -16,7 +17,8 @@ echo "Connected successfully";
 mysqli_set_charset($conn,"utf8");
 
 //of course we need to be careful with user content in our queries
-$sql = "SELECT id, name, created FROM tracks";
+// $sql = "SELECT id, name, created FROM tracks";
+$sql = "SELECT * FROM tracks";
 $result = $conn->query($sql);
 
 echo "<hr>";
@@ -24,7 +26,11 @@ if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
         //for now we use hardcoded keys since we know we have id and name and created
-        echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["created"]. "<br>";
+        $trackText =  "<div class='my-trk'>id: " . $row["id"];
+        $trackText .= " - Name: " . $row["name"];
+        $trackText .= " - Artist: " . $row["artist"];
+        $trackText .=  " " . $row["created"]. "</div>";
+        echo $trackText;
     }
 } else {
     echo "0 results";
--- src/templates/header.php
+++ src/templates/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Fizzbuzz</title>
-    <link rel="stylesheet" href="styles/fizzbuzz.css">
+    <title><?php echo PAGETITLE;?></title>
+    <link rel="stylesheet" href="styles/<?php echo STYLE;?>">
 </head>
 <body>
