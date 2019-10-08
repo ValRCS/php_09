69c2e8b45516a12679e71837f1e1b75893a25706
Valdis Saules <valdis.s.coding@gmail.com>
2019-10-08 10:57:52 +0300

    fixed Class

---
+++ src/classes/House.php
@@ -0,0 +1,12 @@
+<?php
+    class House {
+        //do stuff, properties and methods follow here
+        public $primaryColor = 'black';
+        public $secondaryColors = [
+            'bathroom' => 'white',
+            'bedroom' => 'light pink',
+            'kitchen' => 'light blue'
+        ];
+        public $hasPool = false;
+        public $extra;
+    }
