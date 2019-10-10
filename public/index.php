0ff5d8eaa55cf6369f5deca14a4b3ac9d0052b4c
Valdis Saules <valdis.s.coding@gmail.com>
2019-10-10 12:41:31 +0300

    added logging examples

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
     session_start();
+    require __DIR__ . '/../vendor/autoload.php';
     require_once("../src/Classes/View.php");
     require_once("../src/Classes/Model.php");
     require_once("../src/Classes/Controller.php");
@@ -7,6 +8,13 @@
     $myview = new View();
     $mymodel = new Model($myview);//here DB connection will be created
     $mycontroller = new Controller($mymodel);
+
+    $log = new Monolog\Logger('name');
+    $log->pushHandler(new Monolog\Handler\StreamHandler('../logs/my.log', Monolog\Logger::WARNING));
+    
+    // add records to the log
+    $log->warning('Foo');
+    $log->error('Bar');
 
     $mycontroller->route();
     // $myview->render();
---
+++ public/testMonolog.php
@@ -0,0 +1,15 @@
+<?php
+    echo __DIR__;
+    require __DIR__ . '/../vendor/autoload.php';
+
+    use Monolog\Logger;
+    use Monolog\Handler\StreamHandler;
+    
+    // create a log channel
+    $log = new Logger('name');
+    $log->pushHandler(new StreamHandler('../logs/my.log', Logger::WARNING));
+    
+    // add records to the log
+    $log->warning('Foo');
+    $log->error('Bar');
+    echo "Testing Monolog";
